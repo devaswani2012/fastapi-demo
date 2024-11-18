54cce0c68da05e3ca35174d297eb2a80b90d2f98
krishin aswani <krishinaswani@krishins-MacBook-Air.local>
2024-11-17 21:19:33 -0500

    new endpoints

--- app/main.py
+++ app/main.py
@@ -9,6 +9,13 @@ import json
 import os
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")  # zone apex
 def zone_apex():
@@ -29,3 +36,52 @@ def info():
         "version": "1.0",
         "description": "This is a demo FastAPI app with arithmetic functions."
     }
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+@app.get('/songs')
+def get_songs():
+    query = """
+    SELECT 
+        songs.title, 
+        songs.album, 
+        songs.artist, 
+        songs.year, 
+        songs.file, 
+        songs.image, 
+        genres.genre 
+    FROM songs
+    JOIN genres ON songs.genre = genres.genreid
+    ORDER BY songs.id;
+    """
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(headers, result)))
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "ds2022"
+DBPASS = os.getenv('DBPASS')
+DB = "vzu3vu"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
